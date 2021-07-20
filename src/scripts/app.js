e28fa40baa69f974d74a3509ce0141c8cb6a864f
Alex Karpovich <akarpovich@b2broker.net>
2021-07-20 10:34:33 +0300

    change blocks

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,34 @@
-import log from "./modules/sample-module";
-
-console.log("scrips are compiled successfully!");
-
-log();
+// import log from "./modules/sample-module";
 
 
+// console.log("scrips are compiled successfully!");
+//
+// log();
 
 
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* Текст */
-var speed = 50; /* Скорость/длительность эффекта в миллисекундах */
+// import mainScroll from "./modules/main-screen";
+//
+// console.log("scrips are compiled successfully!");
+//
+// mainScroll();
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
+import mobileMenu from "./modules/main-screen";
+
+
+mobileMenu();
+
+
+
+
+//s
+// var i = 0;
+// var txt = 'Lorem ipsum typing effect!'; /* Текст */
+// var speed = 50; /* Скорость/длительность эффекта в миллисекундах */
+//
+// function typeWriter() {
+//     if (i < txt.length) {
+//         document.getElementById("demo").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
---
+++ src/scripts/modules/main-screen.js
@@ -0,0 +1,20 @@
+// function mainScroll() {
+//     console.log("modules are bundled nicely!");
+// }
+//
+// export default mainScroll;
+
+
+function mobileMenu() {
+    // let menu = document.getElementById("menu");
+
+    // if (x.style.display === "block") {
+    //     x.style.display = "none";
+    // } else {
+    //     x.style.display = "block";
+    // }
+    let mobile = document.querySelector('.mobile');
+    console.log(mobile);
+}
+
+export default mobileMenu;
